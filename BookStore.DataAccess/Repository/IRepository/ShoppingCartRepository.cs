6e086ceb304f907fead2c700acf2673e92e099a3
jcbcouch <jcbcouch@gmail.com>
2025-02-17 15:05:11 -0600

    add shopping cart repo with unit of work pattern

---
+++ BookStore.DataAccess/Repository/IRepository/ShoppingCartRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.DataAccess.Data;
+using BookStore.Models;
+
+namespace BookStore.DataAccess.Repository.IRepository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
