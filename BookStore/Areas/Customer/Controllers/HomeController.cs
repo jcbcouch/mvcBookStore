bac4bf2477edcaa2b110cdf6de2c2fcd828bd96f
jcbcouch <jcbcouch@gmail.com>
2025-02-17 15:32:43 -0600

    add to cart

--- BookStore/Areas/Customer/Controllers/HomeController.cs
+++ BookStore/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using BookStore.DataAccess.Repository.IRepository;
 using BookStore.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BookStore.Areas.Customer.Controllers
 {
@@ -32,6 +34,21 @@ namespace BookStore.Areas.Customer.Controllers
                 ProductId = productId
             };
             return View(cart);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            _unitOfWork.ShoppingCart.Add(shoppingCart);
+            _unitOfWork.Save();
+
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
