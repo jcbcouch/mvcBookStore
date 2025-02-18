d278fc7ffdacf9dfd6dc6f563a9e20defa0b46e9
jcbcouch <jcbcouch@gmail.com>
2025-02-17 20:02:24 -0600

    added shopping cart page

---
+++ BookStore/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Areas.Customer.Controllers
+{
+    [Area("customer")]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
+
