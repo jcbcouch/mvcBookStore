3acbe2e69327d1b877a7f34d8f810bac8236d658
jcbcouch <jcbcouch@gmail.com>
2025-02-23 15:33:32 -0600

    added page for oder summary

--- BookStore/Areas/Customer/Controllers/CartController.cs
+++ BookStore/Areas/Customer/Controllers/CartController.cs
@@ -41,6 +41,10 @@ namespace BookStore.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        public IActionResult Summary()
+        {
+            return View();
+        }
 
         public IActionResult Plus(int cartId)
         {
