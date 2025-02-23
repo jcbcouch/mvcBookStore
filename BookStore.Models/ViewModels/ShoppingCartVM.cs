bafa13a72a47633af6ffbe1375ff11debde78192
jcbcouch <jcbcouch@gmail.com>
2025-02-23 16:29:21 -0600

    add orderheader to cart viewmodel

--- BookStore.Models/ViewModels/ShoppingCartVM.cs
+++ BookStore.Models/ViewModels/ShoppingCartVM.cs
@@ -9,7 +9,7 @@ namespace BookStore.Models.ViewModels
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+        public OrderHeader OrderHeader { get; set; }
 
-        public double OrderTotal { get; set; }
     }
 }
